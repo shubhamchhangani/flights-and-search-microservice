f500156b660057bcbc3d1b8f7b0a01ab88d10cfa
Shubham Chhangani <shubhamchhangani1998@gmail.com>
2024-04-06 15:56:49 +0530

    added filters to getAllCities

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,4 +1,5 @@
 const { City } = require("../models/index.js");
+const { Op } = require("sequelize");
 
 class CityRepository {
   async createCity({ name }) {
@@ -47,8 +48,18 @@ class CityRepository {
     }
   }
 
-  async getAllCities() {
+  async getAllCities(filter) {
     try {
+      if (filter.name) {
+        const cities = await City.findAll({
+          where: {
+            name: {
+              [Op.startsWith]: filter.name,
+            },
+          },
+        });
+        return cities;
+      }
       const cities = await City.findAll();
       return cities;
     } catch (error) {
